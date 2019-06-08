4edc8caadbfcd72c32a524e9adb226c420bf8ddf
Huw Rowlands <huw.rowlands@ieee.org>
2019-06-08 17:23:44 +1000

    Split view supplements back button

--- iOS/AppDelegate.swift
+++ iOS/AppDelegate.swift
@@ -31,21 +31,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UISplitViewControllerDele
 
             listVC.delegate = markerVC
             markerVC.delegate = listVC
+
+            markerVC.navigationItem.leftItemsSupplementBackButton = true
+            markerVC.navigationItem.leftBarButtonItem = splitViewController.displayModeButtonItem
         }
 
         return true
     }
-
-    // MARK: - Split view
-
-    func splitViewController(_ splitViewController: UISplitViewController, collapseSecondary secondaryViewController: UIViewController, onto primaryViewController: UIViewController) -> Bool {
-        guard let secondaryAsNavController = secondaryViewController as? UINavigationController else { return false }
-        guard let topAsDetailController = secondaryAsNavController.topViewController as? MarkerViewController else { return false }
-        if topAsDetailController.selectedMarker == nil {
-            // Return true to indicate that we have handled the collapse by doing nothing; the secondary controller will be discarded.
-            return true
-        }
-        return false
-    }
-
 }
